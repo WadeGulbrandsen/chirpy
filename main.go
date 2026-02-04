9939bd9c8ca5122db5b73d68a9803e892cdd4138
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2026-02-04 17:59:41 -0500

    Added healthz endpoint

--- main.go
+++ main.go
@@ -11,12 +11,17 @@ const (
 
 func main() {
 	mux := http.NewServeMux()
-	dir := http.Dir(".")
-	mux.Handle("/", http.FileServer(dir))
+	appDir := http.Dir(".")
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(appDir)))
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	log.Printf("Serving files from %s on port: %s\n", dir, port)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	log.Printf("Serving files from %s on port: %s\n", appDir, port)
 	log.Fatal(server.ListenAndServe())
 }
